890ae642e4064a5d16895c8f6b4d93b1ec2979e9
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2024-10-14 15:34:38 +0600

    Hero characters animations added
    - Animations not integrated with the movements yet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 movement;
 
+    private bool isFacingRight = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,10 +18,27 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (movement.x < 0 && isFacingRight)
+        {
+            Flip();
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
